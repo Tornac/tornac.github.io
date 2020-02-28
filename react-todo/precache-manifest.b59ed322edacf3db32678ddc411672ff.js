e086aa61d24073880c8fee92bfbe889b7c7a3f82
Tornac <martin.volkmann96@googlemail.com>
2020-02-28 13:18:09 +0100

    attempt to deploy a react app to gh pages

---
+++ react-todo/precache-manifest.b59ed322edacf3db32678ddc411672ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35eed5908444466bbbb7709a28e63352",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec0a6294980b157bc7e7",
+    "url": "/static/css/main.6f89ecc1.chunk.css"
+  },
+  {
+    "revision": "6c852a02aaf98bb0e612",
+    "url": "/static/js/2.d78edd50.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.d78edd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0a6294980b157bc7e7",
+    "url": "/static/js/main.c2b95d7d.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
